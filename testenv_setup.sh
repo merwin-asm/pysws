911a4cbf9d7e1fd383fcf063dd6bfd02fe7bba4a
merwin <85914469+merwin-asm@users.noreply.github.com>
2025-05-11 23:03:41 +0530

    Add files via upload

--- testenv_setup.sh
+++ testenv_setup.sh
@@ -1,9 +1,23 @@
+sed '3,$ s/^#//' /etc/apk/repositories > /etc/apk/repositories
 apk update
 apk add python3 
 apk add py3-pip
 apk add git
 
+cp inittab_new /etc/inittab
+
 git clone https://github.com/merwin-asm/pysws.git
+
+
+# setting up auto start for 'env_sync.py'
+cp env_sync.py /root/env_sync.py
+echo "python3 /root/env_sync.py" > ~/.profile
+
+# installing requirements from 'requirments.txt'
+pip3 install -r requirements.txt --break-system-packages
 cd pysws
-cd dev_env
-python3 env_setup.py # installs pip and other reqs and enables the testrun.sh operations
+pip3 install -r requirements.txt --break-system-packages
+
+# reboot
+reboot
+~
